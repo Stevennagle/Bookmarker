c9ff275f03b8e55b5a3ec14f94eb1cf612a3e272
Steven Nagle <sanagle@ymail.com>
2019-03-31 21:34:43 -0400

    JS read/write localStorage 24:00 min

--- js/main.js
+++ js/main.js
@@ -12,15 +12,32 @@ function saveBookmark(e) {
 	var bookmark = {
 		name: siteName,
 		url: siteUrl
+	    }
+	
+	//console.log(bookmark)
 
+	/*
+		//Local Storage Test
+		localStorage.setItem('test','Hello Internet');
+		console.log(localStorage.getItem('test'));
+
+		localStorage.removeItem('test');
+		console.log(localStorage.getItem('test'));
+	*/
+
+	if(localStorage.getItem('bookmarks') === null){
+
+		var bookmarks = []; 
+		bookmarks.push(bookmark);
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+	}else{
+
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+		bookmarks.push(bookmark);
 	}
-
-	console.log(bookmark)
-
 
 
 
 	e.preventDefault();
-
-
 }
