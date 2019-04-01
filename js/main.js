d3f69bb1cb26d9bcdeed2c7c0fa863cbeb2af70c
Steven Nagle <sanagle@ymail.com>
2019-04-01 18:52:17 -0400

    +local temp storage & retrieval with display

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-
 //Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 function saveBookmark(e) {
-	//console.log(''); 
+	var siteName = document.getElementById('siteName').value;
+	var siteUrl = document.getElementById('siteUrl').value;
 	
-	//get form values
-	var siteName =document.getElementById('siteName').value;
-	var siteUrl =document.getElementById('siteUrl').value;
-
 	var bookmark = {
 		name: siteName,
 		url: siteUrl
-	    }
-	
-	//console.log(bookmark)
-
+	}	
 	/*
 		//Local Storage Test
 		localStorage.setItem('test','Hello Internet');
@@ -24,20 +17,32 @@ function saveBookmark(e) {
 		localStorage.removeItem('test');
 		console.log(localStorage.getItem('test'));
 	*/
-
 	if(localStorage.getItem('bookmarks') === null){
-
 		var bookmarks = []; 
 		bookmarks.push(bookmark);
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-
-	}else{
-
+	}
+	else{
 		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 		bookmarks.push(bookmark);
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
-
-
-
 	e.preventDefault();
 }
+
+function fetchBookmarks(){
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	//console.log(bookmarks);
+	var bookmarksResults = document.getElementById('bookmarksResults');
+
+	bookmarksResults.innerHTML = '';
+	for(var i = 0; i < bookmarks.length; i++) {
+		var name = bookmarks[i].name;
+		var url = bookmarks[i].url;
+		bookmarksResults.innerHTML += '<div class="well">'+
+									   '<h3>'+name+
+									   '</h3>'+
+									   '</div>';
+	}
+}
+
