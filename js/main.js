eac1c2914e5620de23200ff25a88a84e07ae006c
Steven Nagle <sanagle@ymail.com>
2019-04-01 21:31:10 -0400

    Project Completed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-//Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 function saveBookmark(e) {
 	var siteName = document.getElementById('siteName').value;
 	var siteUrl = document.getElementById('siteUrl').value;
 	
-	if (!siteName||!siteUrl) {
-		alert('Please enter both pieces of data');
+	if (!validateForm(siteName, siteUrl)){
 		return false;
 	}
 
@@ -32,6 +30,7 @@ function saveBookmark(e) {
 		bookmarks.push(bookmark);
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
+	document.getElementById('myForm').reset();
 	fetchBookmarks();
 	e.preventDefault();
 }
@@ -67,5 +66,21 @@ function fetchBookmarks(){
 									   '</h3>'+
 									   '</div>';
 	}
-}
+}	
 
+function validateForm(siteName, siteUrl){
+
+	if (!siteName||!siteUrl) {
+		alert('Please enter both pieces of data');
+		return false;
+	}
+
+	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	var regex = new RegExp(expression);
+
+	if (!siteUrl.match(regex)) {
+		alert('Please use a valid URL');
+		return false;		
+	}
+	return true;
+}
