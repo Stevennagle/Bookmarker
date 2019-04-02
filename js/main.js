6810c1ad33cef27e1126d20a26e0b6524366b3e1
Steven Nagle <sanagle@ymail.com>
2019-04-01 21:34:01 -0400

    Project Completed

--- js/main.js
+++ js/main.js
@@ -59,12 +59,13 @@ function fetchBookmarks(){
 	for(var i = 0; i < bookmarks.length; i++) {
 		var name = bookmarks[i].name;
 		var url = bookmarks[i].url;
-		bookmarksResults.innerHTML += '<div class="well">'+
-									   '<h3>'+name+
-									   ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
-									   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
-									   '</h3>'+
-									   '</div>';
+		bookmarksResults.innerHTML += 
+		   '<div class="well">'+
+		   '<h3>'+name+
+		   ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+		   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
+		   '</h3>'+
+		   '</div>';
 	}
 }	
 
